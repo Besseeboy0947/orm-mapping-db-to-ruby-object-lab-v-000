4ba08d1890cdc5de0963d534cd784148b79aebe3
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2019-05-17 05:06:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ end
  
  def self.students_below_12th_grade
     sql = <<-SQL
-    SELECT * FROM students WHERE grade = < 12;
+    SELECT * FROM students WHERE grade = < 11;
     SQL
     
     DB[:conn].execute(sql)
