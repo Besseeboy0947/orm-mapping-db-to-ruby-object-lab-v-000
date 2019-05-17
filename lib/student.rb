5f18ec5828a379f13f437171a56afe3cb498f6d7
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2019-05-17 05:47:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -109,5 +109,9 @@ end
     sql = "SELECT * FROM students WHERE grade = ?"
     DB[:conn].execute(sql, x)
   end
+  def self.first_x_students_in_grade_10(x)
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
+    DB[:conn].execute(sql, x)
+  end
   
 end
