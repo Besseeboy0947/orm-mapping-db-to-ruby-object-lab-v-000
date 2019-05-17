9f5753c3b14f689e5d27fa9687247b8907baa899
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2019-05-17 05:21:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,14 +25,16 @@ class Student
       self.new_from_db(row)
   end.first
 end
+ 
  def self.all
-    SELECT * 
+   sql = <<-SQL
+   SELECT * 
     FROM students 
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
-    end
-  end	  end
+     end
+   end	  
 
   
   def save
